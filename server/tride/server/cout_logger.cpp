56e62670651f686deff67cce519471b00bc1e1cb
dnovikoff <novikov.dmitri.a@gmail.com>
2013-05-26 23:10:04 +0400

    thread_id added to cout_logger

--- server/tride/server/cout_logger.cpp
+++ server/tride/server/cout_logger.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/locks.hpp>
+#include <boost/thread.hpp>
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <tride/log/logger.hpp>
@@ -13,7 +14,7 @@ namespace tride {
 void CoutLogger::doWrite(const log::LogLevel level, const std::string& msg) {
 	static boost::mutex writeMutex;
 	boost::mutex::scoped_lock l(writeMutex);
-	std::cout << '[' << boost::posix_time::second_clock::local_time() << " " << logLevelName(level) << "] " << msg << std::endl;
+	std::cout << '[' << boost::posix_time::second_clock::local_time() << " " << boost::this_thread::get_id() << " "<< logLevelName(level) << "] " << msg << std::endl;
 }
 
 }  // namespace tride
